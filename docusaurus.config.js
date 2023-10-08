c2b13a514e19690ec3b69ce9c87438663ee19da1
Boda Bálint <68827048+Valentinusz@users.noreply.github.com>
2023-10-08 16:35:11 +0200

    add katex, mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,30 +4,30 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'Boda Bálint',
     favicon: 'img/favicon.ico',
 
-    // Set the production url of your site here
     url: 'https://valentinusz.github.io',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
     trailingSlash: false,
     deploymentBranch: 'gh-pages',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'valentinusz', // Usually your GitHub org/user name.
-    projectName: 'valentinusz.github.io', // Usually your repo name.
+    organizationName: 'valentinusz',
+    projectName: 'valentinusz.github.io',
+
+    markdown: {
+        mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
 
-    // Even if you don't use internalization, you can use this field to set useful
-    // metadata like html lang. For example, if your site is Chinese, you may want
-    // to replace "en" with "zh-Hans".
     i18n: {
         defaultLocale: 'hu',
         locales: ['hu'],
@@ -42,13 +42,12 @@ const config = {
                     routeBasePath: 'materials',
                     sidebarPath: require.resolve('./sidebars.js'),
                     showLastUpdateAuthor: true,
-                    showLastUpdateTime: true
+                    showLastUpdateTime: true,
+                    remarkPlugins: [math],
+                    rehypePlugins: [katex]
                 },
                 blog: {
                     showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
@@ -56,9 +55,18 @@ const config = {
             }),
         ],
     ],
+    stylesheets: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity:
+                'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+        },
+    ],
 
     themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
             // Replace with your project's social card
             image: 'img/docusaurus-social-card.jpg',
@@ -84,27 +92,14 @@ const config = {
                 ],
             },
             algolia: {
-                // The application ID provided by Algolia
                 appId: 'C09RZLQGSR',
-
-                // Public API key: it is safe to commit it
                 apiKey: '8a7a4806d9bfdfacfb3a9f7be78038b7',
-
                 indexName: 'valentinuszio',
-
-                // Optional: see doc section below
                 contextualSearch: true,
-
-                // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-                // externalUrlRegex: 'external\\.com|domain\\.com',
-
-                // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
                 replaceSearchResultPathname: {
-                    from: '/materials/', // or as RegExp: /\/docs\//
+                    from: '/materials/',
                     to: '/',
                 },
-
-                // Optional: path for search page that enabled by default (`false` to disable it)
                 searchPagePath: 'search',
             },
 
